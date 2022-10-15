67ba21d47ffcccdc04ba4913c414ec9ade37641b
Erika <erika.duarte.mora@gmail.com>
2022-10-15 14:16:13 -0500

    funciones de acceder a data, favicon e imgs

--- src/data.js
+++ src/data.js
@@ -1,9 +1,40 @@
-// estas funciones son de ejemplo
+import pokemon from './data/pokemon/pokemon.js';
+const data = pokemon;
+const arrayD = data.pokemon;
 
-export const example = () => {
-  return 'example';
-};
+export function personajes(pokemon){
+  return [pokemon.name, pokemon.img, pokemon.resistant, 
+    pokemon.weaknesses, pokemon.encounter["base-capture-rate"], 
+    pokemon.encounter["base-flee-rate"]]
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+/*export function fortaleza(pokemon){
+  return [pokemon.resistant];
+}
+
+export function debil(pokemon){
+  return [pokemon.weaknesses];
+}*/
+  //Filtra la data
+/*filterData(data, condition) {
+
+  }
+//ordena la data
+  sortData(data, sortBy, sortOrder)
+  arrayD.sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      if (nameA < nameB){
+        return -1;
+      }
+      if (nameA > nameB){
+        return 1;
+      } else {
+        return 0;
+      }
+    }); 
+
+//calculos estadisticos
+  computeStats(data)
+}*/
+export default pokemon;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import pokemon from './data.js';
+import {personajes} from './data.js';
 
-console.log(example, data);
+const data = pokemon;
+const arrayD = data.pokemon;
+ //arrayD.forEach(pokemon => pokemon[]) 
+ 
+ let pokemones = arrayD.map(personajes).join(" ");
+ console.log(arrayD);
+ 
+
+    
+ 
+ 
+ /*let filtro = pokemon.name;
+ function filterData(pokemones, filtro) {
+    if(filtro === pokemones){
+        return pokemones[filtro];
+    } else if (filtro != pokemones){
+       return console.log("El nombre del pokemon no existe");
+    }
+ };
+ console.log(filterData());*/
+ 
+   
+    
+
+ 
+
+ document.querySelector(".tarjetas").innerHTML= pokemones;
+ 
